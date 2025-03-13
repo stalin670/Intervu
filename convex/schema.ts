5ed840dc31f7aec3a4fcfdcf810b6e6d5b875fd0
stalin670 <amitalvin01@gmail.com>
2025-03-13 16:10:46 +0530

    created interviews-schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -8,5 +8,18 @@ export default defineSchema({
         image: v.optional(v.string()),
         role: v.union(v.literal("candidate"), v.literal("interviewer")),
         clerkId: v.string(),
-    }).index("by_clerk_id", ["clerkId"])
+    }).index("by_clerk_id", ["clerkId"]),
+
+    interviews: defineTable({
+        title: v.string(),
+        description: v.optional(v.string()),
+        startTime: v.number(),
+        endTime: v.optional(v.number()),
+        status: v.string(),
+        streamCallId: v.string(),
+        candidateId: v.string(),
+        interviewerIds: v.array(v.string()),
+    })
+        .index("by_candidate_id", ["candidateId"])
+        .index("by_stream_call_id", ["streamCallId"]),
 });
