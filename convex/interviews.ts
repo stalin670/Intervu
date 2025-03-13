a0bafe75a63be33085a7949cb5c1c6dc893e697e
stalin670 <amitalvin01@gmail.com>
2025-03-13 16:48:01 +0530

    implemented getAllInterviews controller

---
+++ convex/interviews.ts
@@ -0,0 +1,27 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const getAllInterviews = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) throw new Error("Unauthorized");
+
+        const interviews = await ctx.db.query("interviews").collect();
+
+        return interviews;
+    },
+});
+
+export const getMyInterviews = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) return [];
+
+        const interviews = await ctx.db
+            .query("interviews")
+            .withIndex("by_candidate_id", (q) => q.eq("candidateId", identity.subject))
+            .collect();
+
+        return interviews!;
+    },
+});
