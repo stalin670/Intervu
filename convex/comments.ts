a1a63b00af155c81e295ccf3e4e2701ec355ebdd
stalin670 <amitalvin01@gmail.com>
2025-03-13 16:34:27 +0530

    implemented add comment functionality

---
+++ convex/comments.ts
@@ -0,0 +1,21 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const addComment = mutation({
+    args: {
+        interviewId: v.id("interviews"),
+        content: v.string(),
+        rating: v.number(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) throw new Error("Unauthorized");
+
+        return await ctx.db.insert("comments", {
+            interviewId: args.interviewId,
+            content: args.content,
+            rating: args.rating,
+            interviewerId: identity.subject,
+        });
+    },
+});
