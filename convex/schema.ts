b4c8eb237377b724aa42a90fe6a9f3d34479a80e
stalin670 <amitalvin01@gmail.com>
2025-03-13 16:14:57 +0530

    created comment-schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -22,4 +22,11 @@ export default defineSchema({
     })
         .index("by_candidate_id", ["candidateId"])
         .index("by_stream_call_id", ["streamCallId"]),
+
+    comments: defineTable({
+        content: v.string(),
+        rating: v.number(),
+        interviewerId: v.string(),
+        interviewId: v.id("interviews"),
+    }).index("by_interview_id", ["interviewId"]),
 });
