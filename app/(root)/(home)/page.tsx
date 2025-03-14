98b0ae5f8dc1eb42feca27c597c6a15ca9f4c0bf
stalin670 <amitalvin01@gmail.com>
2025-03-14 21:13:37 +0530

    update UI text-color

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <div className="container max-w-7xl mx-auto p-6">
       <div className="rounded-lg bg-card p-6 border shadow-sm mb-10">
-        <h1 className="text-4xl font-bold bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
+        <h1 className="text-4xl font-bold bg-gradient-to-r bg-clip-text">
           Welcome back!
         </h1>
         <p className="text-muted-foreground mt-2">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,18 +9,16 @@ const Navbar = () => {
     return (
         <nav className="border-b">
             <div className="flex h-16 items-center px-4 container mx-auto">
-                {/* LEFT SIDE -LOGO */}
                 <Link
                     href="/"
                     className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
                 >
-                    <CodeIcon className="size-8 text-emerald-500" />
-                    <span className="bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
+                    <CodeIcon className="size-8 text-black-500" />
+                    <span className="bg-gradient-to-r">
                         Intervu
                     </span>
                 </Link>
 
-                {/* RIGHT SIDE - ACTIONS */}
                 <SignedIn>
                     <div className="flex items-center space-x-4 ml-auto">
                         <DasboardBtn />
