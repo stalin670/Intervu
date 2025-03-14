133a69cba872929aa6607fa6f678e100250cae2e
stalin670 <amitalvin01@gmail.com>
2025-03-14 22:58:05 +0530

    changes default to interviewer instead of candidate

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,7 +6,7 @@ export default defineSchema({
         name: v.string(),
         email: v.string(),
         image: v.optional(v.string()),
-        role: v.union(v.literal("candidate"), v.literal("interviewer")),
+        role: v.union(v.literal("interviewer"), v.literal("candidate")),
         clerkId: v.string(),
     }).index("by_clerk_id", ["clerkId"]),
 
