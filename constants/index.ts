18f7bc333b0db028f776723a905a50a1c3b8acce
stalin670 <amitalvin01@gmail.com>
2025-03-14 19:12:54 +0530

    added c++ language for code and png

--- constants/index.ts
+++ constants/index.ts
@@ -89,6 +89,9 @@ export const CODING_QUESTIONS: CodeQuestion[] = [
         
     }
 }`,
+            cpp: `int twoSum(vector<int>& nums, int target) {
+    // Write your solution here
+}`,
         },
         constraints: [
             "2 ≤ nums.length ≤ 104",
@@ -126,6 +129,9 @@ export const CODING_QUESTIONS: CodeQuestion[] = [
         
     }
 }`,
+            cpp: `void reverseString(string & str) {
+    // Write your solution here
+}`,
         },
     },
     {
@@ -160,6 +166,9 @@ export const CODING_QUESTIONS: CodeQuestion[] = [
         
     }
 }`,
+            cpp: `bool isPalindrome(int num) {
+    // Write your solution here
+}`,
         },
     },
 ];
@@ -168,6 +177,7 @@ export const LANGUAGES = [
     { id: "javascript", name: "JavaScript", icon: "/javascript.png" },
     { id: "python", name: "Python", icon: "/python.png" },
     { id: "java", name: "Java", icon: "/java.png" },
+    { id: "cpp", name: "C++", icon: "/cpp.png" },
 ] as const;
 
 export interface CodeQuestion {
@@ -183,6 +193,7 @@ export interface CodeQuestion {
         javascript: string;
         python: string;
         java: string;
+        cpp: string;
     };
     constraints?: string[];
 }
